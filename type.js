8a5f5601f538427275c7a8ccceb70805878e2b06
ElnaraSadiqli <elnaragk@code.edu.az>
2022-05-02 15:43:31 +0400

    "SetTime Dynamic"

--- type.js
+++ type.js
@@ -1,10 +1,22 @@
-// let time = document.querySelector("#time");
+// Assignment
+
 let hour = document.querySelector("#hour");
 let minute = document.querySelector("#minute");
 let second = document.querySelector("#second");
-
 let time = new Date();
+timer();
 
-hour.innerHTML = time.getHours();
-minute.innerHTML = time.getMinutes();
-second.innerHTML = time.getSeconds();
+
+function timer() {
+
+    hour.innerHTML = time.getHours();
+    minute.innerHTML = time.getMinutes();
+    second.innerHTML = time.getSeconds();
+    displayTime(e);
+}
+
+function displayTime(e) {
+
+    e.preventDefault;
+    setTimeout("timer()", 1000);
+}
