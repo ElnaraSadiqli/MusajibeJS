8de34361683f4f0da74b58ce75096335df6b77da
ElnaraSadiqli <elnaragk@code.edu.az>
2022-05-03 22:07:53 +0400

    "Clock Auto change seconds"

--- type.js
+++ type.js
@@ -3,20 +3,31 @@
 let hour = document.querySelector("#hour");
 let minute = document.querySelector("#minute");
 let second = document.querySelector("#second");
-let time = new Date();
-timer();
+let session = document.querySelector(".sessions");
+
+setInterval(timer, 1000);
+
+
+
+function setZero() {
+
+    if (minute < 10) {
+
+        minute.innerHTML = "0" + minute;
+
+    } if (second < 10) {
+
+        second.innerHTML = "0" + second;
+    }
+}
 
 
 function timer() {
+    let time = new Date();
 
     hour.innerHTML = time.getHours();
     minute.innerHTML = time.getMinutes();
     second.innerHTML = time.getSeconds();
-    displayTime(e);
-}
 
-function displayTime(e) {
-
-    e.preventDefault;
-    setTimeout("timer()", 1000);
+    setZero();
 }
