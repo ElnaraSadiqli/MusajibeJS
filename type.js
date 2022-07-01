1bff35ced8b0727b339f7b2f43caa9d1e1f24d99
ElnaraSadiqli <elnaragk@code.edu.az>
2022-07-01 13:29:41 +0400

    https://github.com/ElnaraSadiqli/todolist.git

--- type.js
+++ type.js
@@ -1,33 +1,4 @@
-// Assignment
-
-let hour = document.querySelector("#hour");
-let minute = document.querySelector("#minute");
-let second = document.querySelector("#second");
-let session = document.querySelector(".sessions");
-
-setInterval(timer, 1000);
 
 
 
-function setZero() {
 
-    if (minute < 10) {
-
-        minute.innerHTML = "0" + minute;
-
-    } if (second < 10) {
-
-        second.innerHTML = "0" + second;
-    }
-}
-
-
-function timer() {
-    let time = new Date();
-
-    hour.innerHTML = time.getHours();
-    minute.innerHTML = time.getMinutes();
-    second.innerHTML = time.getSeconds();
-
-    setZero();
-}
