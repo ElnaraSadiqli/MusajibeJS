17ba82319485122d9e9471b8dbc0fb65915feb1e
ElnaraSadiqli <elnaragk@code.edu.az>
2022-07-05 12:35:10 +0400

    Js configuration

--- type.js
+++ type.js
@@ -1,4 +1,43 @@
+// Select Html elements
+let input = document.querySelector(".list-input");
+let buttonIn = document.querySelector("#btnInsert");
+let buttonDel = document.querySelector(".del-btn");
+let listAll = document.querySelector("#list-all");
+let li;
 
 
+eventListeners();
+
+function eventListeners() {
+
+    buttonIn.addEventListener("click", addEvent);
+}
 
 
+function addEvent(event) {
+
+    condition();
+
+    event.preventDefault;
+}
+
+function condition(event) {
+
+ if (isNaN(input.value) === false || input.value == " " ) {
+        alert("Please Insert String");
+    } else {
+        li = `<li class="list-group-item">
+        <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+      ${input.value}
+    </li>`
+
+        listAll.insertAdjacentHTML("beforeEnd", li);
+
+        input.value = " ";
+    }
+
+    event.preventDefault;
+
+
+}
+console.log(condition());
